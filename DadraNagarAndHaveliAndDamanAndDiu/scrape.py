c2b53ed90321385f493b6f24d956f97bced0081a
ramSeraph <kandimalla.sreeram@gmail.com>
2024-06-18 13:10:09 +0530

    add Dadra and Nagar Haveli and Daman and Diu scraper

---
+++ DadraNagarAndHaveliAndDamanAndDiu/scrape.py
@@ -0,0 +1,86 @@
+import json
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+
+main_url = 'https://ddd.gov.in/document-category/official-gazette/'
+
+def get_page_count(session):
+    print(f'pulling page count from {main_url}')
+    resp = session.get(main_url)
+    if not resp.ok:
+        raise Exception(f'unable to retrieve {main_url}')
+    html = resp.text
+    soup = BeautifulSoup(html, 'html.parser')
+    main = soup.find('main')
+    pagination_div = main.find('div', {'class': 'pagination'})
+    lis = pagination_div.find_all('li', {'class': ''})
+    if len(lis) == 0:
+        return 1
+    last_li = lis[-1]
+    last_link = last_li.find('a').attrs['href'] 
+    last_page_id = int(last_link.split('/')[-1])
+    return last_page_id
+
+def get_per_page_infos(session, pno):
+    page_dir = Path('data/pages/')
+    page_dir.mkdir(exist_ok=True)
+    page_file = page_dir / f'{pno}.json'
+    if page_file.exists():
+        return json.loads(page_file.read_text())
+    page_url = main_url + f'page/{pno}'
+    print(f'pulling info from {page_url}')
+    resp = session.get(page_url)
+    if not resp.ok:
+        raise Exception(f'unable to retrieve {page_url}')
+    html = resp.text
+    soup = BeautifulSoup(html, 'html.parser')
+    main = soup.find('main')
+    table = main.find('table', {'class': 'doc-table'})
+    tbody = table.find('tbody')
+    trs = tbody.find_all('tr', recursive=False)
+    infos = []
+    for tr in trs:
+        tds = tr.find_all('td', recursive=False)
+        a = tds[0].find('a')
+        name = a.text.strip()
+        url = a.attrs['href']
+        date = tds[1].text.strip()
+        info = { 'name': name, 'date': date, 'url': url }
+        infos.append(info)
+    page_file.write_text(json.dumps(infos))
+    return infos
+
+
+def get_infos(session):
+    page_count = get_page_count(session)
+    infos = []
+    for i in range(1, page_count + 1):
+        infos += get_per_page_infos(session, i)
+    return infos
+
+def download_gazette(session, info):
+    gaz_dir = Path('data/gazettes')
+    gaz_dir.mkdir(exist_ok=True)
+    url = info['url']
+    parts = url.split('/')
+    idx = parts.index('uploads')
+    fname = '_'.join(parts[idx+1:])
+    file = gaz_dir / fname
+    if file.exists():
+        return
+    print(f'downloading file from {url}')
+    resp = session.get(url)
+    if not resp.ok:
+        raise Exception(f'unable to retrieve file at {url}')
+    file.write_bytes(resp.content)
+
+
+if __name__ == '__main__':
+    Path('data').mkdir(exist_ok=True)
+    session = requests.session()
+    infos = get_infos(session)
+    for info in infos:
+        download_gazette(session, info)
+
